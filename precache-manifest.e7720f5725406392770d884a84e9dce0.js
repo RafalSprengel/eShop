a6da3b4986ab2727f2408d8d48ab7eecea0cefa1
unknown <sprengel.rafal@gmail.com>
2020-09-29 21:53:32 +0100

    Updates

---
+++ precache-manifest.e7720f5725406392770d884a84e9dce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e08b560cb1553963028b2ab7f826fa8",
+    "url": "/eShop/index.html"
+  },
+  {
+    "revision": "96729865e4aacc3249fa",
+    "url": "/eShop/static/css/main.34f1d581.chunk.css"
+  },
+  {
+    "revision": "d8f4214cbb6bec93c583",
+    "url": "/eShop/static/js/2.d141aeac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/eShop/static/js/2.d141aeac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96729865e4aacc3249fa",
+    "url": "/eShop/static/js/main.e44ead1d.chunk.js"
+  },
+  {
+    "revision": "8821ace3f2d0d214742a",
+    "url": "/eShop/static/js/runtime-main.04127bf1.js"
+  },
+  {
+    "revision": "f8659509e97596b5fda61fd0f4a15c1b",
+    "url": "/eShop/static/media/logo.f8659509.png"
+  }
+]);
