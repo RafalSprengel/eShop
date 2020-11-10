86156f8e0f5f1fa645cb605cb25bb9c9cc4b1dfe
unknown <sprengel.rafal@gmail.com>
2020-11-10 08:50:17 +0000

    changes

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom'
 import { Link, NavLink } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import Button from "@material-ui/core/Button";
 import SwitchBut from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import axios from 'axios';
 import '../styles/checkout.scss'
 import logo from '../pics/logo.png'
 import '../styles/customRadio.scss'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        overflow: 'auto',
+        maxHeight: 300,
+    },
+    listItem: {
+        backgroundColor: 'whitesmoke',
+        border: '2px solid white',
+        cursor: 'pointer'
+    }
+}));
+
 const Stepper = () => {
     return (
         <div className='checkout__stepper'>
@@ -80,11 +95,25 @@ const LoginForm = () => {
 
 const AddressForm = () => {
 
-    const [formFieldsObj, setformFieldsObj] = useState('');
+    const [formFieldsObj, setformFieldsObj] = useState({
+        firstName: {
+            value: ''
+        },
+        lastName: {
+            value: ''
+        },
+        tel: {
+            value: ''
+        },
+        email: {
+            value: ''
+        }
+    });
     const [postcodeInputValue, setPostcodeInputValue] = useState('')
-    const [autComAddrDatBaseTab, setAutComAddrDatBaseTab] = useState('')
-    const [postcodeSelectValue, setPostcodeSelectValue] = useState('')
-
+    const [addressListTab, setAddressListTab] = useState('')
+    const tempAddressBase = [{ "address": ["Flat 2", "286 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "286", "subbuilding": "Flat 2" }, { "address": ["Flat 3", "288 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "288", "subbuilding": "Flat 3" }, { "address": ["Flat 4", "290 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "290", "subbuilding": "Flat 4" }, { "address": ["Flat 4", "292 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "292", "subbuilding": "Flat 4" }, { "address": ["Flat 3", "292 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "292", "subbuilding": "Flat 3" }, { "address": ["Flat 5", "294 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "294", "subbuilding": "Flat 5" }, { "address": ["Flat 3", "296 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "296", "subbuilding": "Flat 3" }, { "address": ["Flat 4", "296 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "296", "subbuilding": "Flat 4" }, { "address": ["Flat 1", "286 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "286", "subbuilding": "Flat 1" }, { "address": ["Flat 5", "286 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "286", "subbuilding": "Flat 5" }, { "address": ["Flat 5", "288 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "288", "subbuilding": "Flat 5" }, { "address": ["Flat 2", "292 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "292", "subbuilding": "Flat 2" }, { "address": ["Flat 5", "292 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "292", "subbuilding": "Flat 5" }, { "address": ["Flat 1", "294 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "294", "subbuilding": "Flat 1" }, { "address": ["Flat 2", "294 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "294", "subbuilding": "Flat 2" }, { "address": ["Flat 5", "296 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "296", "subbuilding": "Flat 5" }, { "address": ["Flat 1", "296 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "296", "subbuilding": "Flat 1" }, { "address": ["308 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "308" }, { "address": ["Flat 4", "286 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "286", "subbuilding": "Flat 4" }, { "address": ["Flat 3", "286 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "286", "subbuilding": "Flat 3" }, { "address": ["Flat 4", "288 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "288", "subbuilding": "Flat 4" }, { "address": ["Flat 2", "288 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "288", "subbuilding": "Flat 2" }, { "address": ["Flat 1", "288 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "288", "subbuilding": "Flat 1" }, { "address": ["Flat 1", "290 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "290", "subbuilding": "Flat 1" }, { "address": ["Flat 5", "290 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "290", "subbuilding": "Flat 5" }, { "address": ["Flat 2", "290 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "290", "subbuilding": "Flat 2" }, { "address": ["Flat 3", "290 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "290", "subbuilding": "Flat 3" }, { "address": ["Flat 1", "292 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "292", "subbuilding": "Flat 1" }, { "address": ["Flat 3", "294 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "294", "subbuilding": "Flat 3" }, { "address": ["Flat 4", "294 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "294", "subbuilding": "Flat 4" }, { "address": ["Flat 2", "296 Beverley Road"], "county": "East Riding of Yorkshire", "postcode": "HU5 1AX", "city": "Hull", "street": "Beverley Road", "class": "residential", "buildingnumber": "296", "subbuilding": "Flat 2" }]
+    const [chosenAddress, setChosenAddress] = useState(null)
+    const classes = useStyles();
     const handleFieldOnChange = (e) => {
         const { value, name } = e.target;
         setformFieldsObj((prev) => ({ ...prev, [name]: { 'value': value } }))
@@ -94,12 +123,12 @@ const AddressForm = () => {
         const { value, name } = e.target;
         const errorMessages = [{
             'fieldName': 'firstName',
-            'regExp': String.raw`^[\w]+$`,
+            'regExp': String.raw`^[\wżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$`,
             'message': 'Please use only a-z, 0-1 and _.'
         },
         {
             'fieldName': 'lastName',
-            'regExp': String.raw`^[\w]+$`,
+            'regExp': String.raw`^[\wżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$`,
             'message': 'Please use only a-z, 0-1 and _.'
         },
         {
@@ -144,18 +173,20 @@ const AddressForm = () => {
         }
         validation()
     }
-    useEffect( //Reading from session Storage
-        () => {
-            if (formFieldsObj === '' && sessionStorage.getItem("checkoutFormData"))
-                setformFieldsObj(JSON.parse(sessionStorage.getItem("checkoutFormData")))
-        }, [])
+    useEffect(() => { //Reading from session Storage
+        if (sessionStorage.getItem("checkoutFormData"))
+            setformFieldsObj(JSON.parse(sessionStorage.getItem("checkoutFormData")))
+
+        if (sessionStorage.getItem("chosenAddress"))
+            setChosenAddress(JSON.parse(sessionStorage.getItem("chosenAddress")))
+    }, [])
 
     useEffect(() => { //Save to sessionStorage
         if (formFieldsObj !== '' && JSON.stringify(formFieldsObj) !== sessionStorage.getItem("checkoutFormData"))
             sessionStorage.setItem("checkoutFormData", JSON.stringify(formFieldsObj));
     }, [formFieldsObj])
 
-    const SingleInput = ({ val, onChangeF, name, label, required }) => {
+    /* const SingleInput = ({ val, onChangeF, name, label, required }) => {
         return (
             <TextField
                 value={val}
@@ -170,7 +201,7 @@ const AddressForm = () => {
                 required={required ? true : false}
             />
         )
-    }
+    } */
 
     /* useEffect(() => {
         if (postcodeInputValue.length < 4) return () => null
@@ -196,11 +227,32 @@ const AddressForm = () => {
             })
             .catch((errors) => console.log(errors));
     }, [postcodeInputValue]); */
+    const getAddress = () => {
+        if (postcodeInputValue.length < 4) return () => null
+        const URL = "https://api.addressian.co.uk/v2/autocomplete/" + postcodeInputValue;
+        const options = {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8',
+                'x-api-key': 'yBdiABu5565T4HMJUVLiKaTa3HyV6Xsp7CVBzjeA'
+            },
 
+        }
+        fetch(URL, options)
+            .then((response) => {
+                if (response.ok) return response;
+                else throw Error(response.statusText);
+            })
+            .then((response) => response.json())
+            .then((response) => {
+                let sorted = response.sort((a, b) => a.buildingnumber - b.buildingnumber);
+                setAddressListTab(sorted)
+            })
+            .catch((errors) => console.log(errors));
+    }
 
-
-    if (autComAddrDatBaseTab) console.log(autComAddrDatBaseTab)
-    useEffect(() => console.log('postcodeSelectValue to : ', postcodeSelectValue), [postcodeSelectValue])
+    useEffect(() => console.log(addressListTab))
     return (
         <>
             <form className='checkout__addressForm'>
@@ -208,8 +260,9 @@ const AddressForm = () => {
                     <p className="checkout__addressForm__title">Your details:</p>
                     <p className="checkout__addressForm__desc" >Please fill in your address details below. This information will be used for your delivery and payment</p>
                 </legend>
+                <p className='checkout__addressForm__starRequired'> Field Required</p>
                 <TextField
-                    value={formFieldsObj.firstName ? formFieldsObj.firstName.value : ''}
+                    value={formFieldsObj.firstName.value}
                     onChange={handleFieldOnChange}
                     onBlur={handleFieldOnBlur}
                     error={formFieldsObj.firstName ? formFieldsObj.firstName.error : false}
@@ -223,7 +276,7 @@ const AddressForm = () => {
                     margin='dense'
                     required />
                 <TextField
-                    value={formFieldsObj.lastName ? formFieldsObj.lastName.value : ''}
+                    value={formFieldsObj.lastName.value}
                     onChange={handleFieldOnChange}
                     onBlur={handleFieldOnBlur}
                     error={formFieldsObj.lastName ? formFieldsObj.lastName.error : false}
@@ -237,7 +290,7 @@ const AddressForm = () => {
                     margin='dense'
                     required />
                 <TextField
-                    value={formFieldsObj.email ? formFieldsObj.email.value : ''}
+                    value={formFieldsObj.email.value}
                     onChange={handleFieldOnChange}
                     onBlur={handleFieldOnBlur}
                     helperText={formFieldsObj.email ? formFieldsObj.email.message : ''}
@@ -251,7 +304,7 @@ const AddressForm = () => {
                     margin='dense'
                     required />
                 <TextField
-                    value={formFieldsObj.tel ? formFieldsObj.tel.value : ''}
+                    value={formFieldsObj.tel.value}
                     onChange={handleFieldOnChange}
                     onBlur={handleFieldOnBlur}
                     helperText={formFieldsObj.tel ? formFieldsObj.tel.message : ''}
@@ -265,19 +318,13 @@ const AddressForm = () => {
                     margin='dense' />
 
                 <legend className='checkout__addressForm__title'>Delivery Address:</legend>
-                <legend className='checkout__addressForm__findAddressField'>Find Address by Postcode</legend>
-                <Autocomplete
-                    autoComplete={true}
-                    autoHighlight={true}
-                    value={postcodeSelectValue}
-                    onChange={(e, newValue) => setPostcodeSelectValue(newValue)}
-                    onInputChange={(e, newValue) => setPostcodeInputValue(newValue)}
-                    noOptionsText={'No addresses found !'}
-                    options={autComAddrDatBaseTab ? (autComAddrDatBaseTab.map((el) => `${el.postcode}, ${el.address}`)) : []}
-                    renderInput={(params) => (
+                {!chosenAddress &&
+                    <div>
+                        <legend className='checkout__addressForm__findAddressText'>Find Address by Postcode</legend>
+
                         <TextField
-                            {...params}
                             autoComplete='off'
+                            onChange={(e) => setPostcodeInputValue(e.target.value)}
                             onBlur={handleFieldOnBlur}
                             helperText={formFieldsObj.postcode ? formFieldsObj.postcode.message : ''}
                             error={formFieldsObj.postcode ? formFieldsObj.postcode.error : false}
@@ -290,17 +337,45 @@ const AddressForm = () => {
                             fullWidth
                             margin='dense'
                             required />
-                    )}
-                />
 
-                {/* <div className='checkout__addressForm__findAddressButton'>
-                    <Button
-                        variant="outlined"
-                        color="primary"
-                        fullWidth
-                    >Find Address
+                        <div className='checkout__addressForm__findPostcodeButton'>
+                            <Button
+                                onClick={getAddress}
+                                variant="outlined"
+                                color="primary"
+                                fullWidth
+                            >Find Address
                 </Button>
-                </div> */}
+                        </div>
+                        {tempAddressBase && //addressListTab
+                            <List className={classes.root}>
+                                {tempAddressBase.map((el, index) => (
+                                    <ListItem
+                                        key={index}
+                                        onClick={() => {
+                                            setChosenAddress(el)
+                                            sessionStorage.setItem("chosenAddress", JSON.stringify(el));
+                                        }}
+                                        className={classes.listItem}>
+                                        {el.postcode}, {el.address.join(', ')}
+                                    </ListItem>))}
+                            </List>
+                        }
+                    </div>
+
+                }
+                {chosenAddress &&
+                    <>
+                        <div>
+                            <p>{chosenAddress.address.map((el) => <p>{el}</p>)}</p>
+                            <p>{chosenAddress.city}</p>
+                            <p>{chosenAddress.postcode}</p>
+                        </div>
+                        <p className='checkout__addressForm__removeAddress' onClick={() => { setChosenAddress(null); sessionStorage.removeItem("chosenAddress") }}>Remove Address</p>
+                    </>
+
+                }
+
                 {/* <TextField
                     value={formFieldsObj.city ? formFieldsObj.city.value : ''}
                     onChange={handleFieldOnChange}
@@ -343,7 +418,6 @@ const AddressForm = () => {
                     color='secondary'
                     fullWidth
                     margin='dense' /> */}
-                <p className='checkout__addressForm__starRequired'> Field Required</p>
             </form>
             <div >
                 <Link to='/checkout/payment' >
